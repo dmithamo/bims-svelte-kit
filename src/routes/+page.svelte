447fcd99fc45c1124f4b703a64bf5fc4099b037f
dmithamo <dennis.mithamo@sportserve.co>
2024-08-23 10:05:31 +0300

    rm unecessary scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Logo from '$lib/components/logo.svelte';
 </script>
 
-<div class="flex flex-col items-center justify-center h-screen gap-2">
+<div class="flex flex-col items-center justify-center h-full gap-2">
 	<h1 class="text-3xl text-aLight dark:text-aDark">Welcome to BIMS</h1>
 	<small>Work in Progress&TRADE;</small>
 	<Logo />
