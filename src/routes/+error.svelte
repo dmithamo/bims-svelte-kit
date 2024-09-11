ac721291a0590b850587dfeb28ac14499c60f97c
dmithamo <33679184+dmithamo@users.noreply.github.com>
2024-09-11 13:38:05 +0300

    fix: text-danger to text-error

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,9 +5,9 @@
 
 <div class="flex flex-col items-center justify-center relative h-full gap-4">
 	<Logo />
-	<h1 class="text-3xl text-danger">Bims | Error {$page.status}</h1>
+	<h1 class="text-3xl text-error">Bims | Error {$page.status}</h1>
 
-	<p class="mt-4 text-danger">
+	<p class="mt-4 text-error">
 		{$page?.error?.message || 'Something went very wrong. Check back later&TRADE;'}
 	</p>
 </div>
